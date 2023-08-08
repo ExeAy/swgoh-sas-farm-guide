2272d28b865ad1319873e0019a6a0966c899ca72
Hannes Falk <hannes.falk@insclear.se>
2023-08-08 16:43:34 +0200

    Adding more explaining text

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,23 @@ import type React from 'react'
 
 const Header: React.FC = () => {
   return (
-    <h1 className="flex justify-center font-bold text-3xl">RoS Farm Guide</h1>
+    <div>
+      <h1 className="flex justify-center font-bold text-5xl">RoS Farm Guide</h1>
+      <p className="text-xl">
+        Denna guide ska ses som en rekommendation för att klara guild-events på
+        bästa sätt.
+      </p>
+      <p className="text-xl mt-4">
+        Vi har inte tagit hänsyn till skepp men här är vår rekommendation:
+        <ul className="list-disc list-inside text-xl">
+          <li>
+            Gå för Executor tidigt. Helt okej att låsa upp den före er första
+            GL.
+          </li>
+          <li>Farma upp Malevolance till 7* före ni farmar upp Negotioator.</li>
+        </ul>
+      </p>
+    </div>
   )
 }
 
