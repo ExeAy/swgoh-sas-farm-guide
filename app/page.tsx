f86185df2339a08ac9a4fe8cf24e322616f54467
Hannes Falk <hannes.falk@insclear.se>
2023-08-12 20:18:08 +0200

    Add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/react";
 import Landing from "../src/components/landing";
 
 export default function Home() {
   return (
     <main>
       <Landing />
+      <Analytics />
     </main>
   );
 }
